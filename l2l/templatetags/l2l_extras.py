0368f7df64d94dbf30a61fd4231b338c7d4d40fe
johnswon <johnwon.us@gmail.com>
2021-06-28 20:24:36 -0400

    Added Decorator

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -7,14 +7,11 @@ register = template.Library()
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 ISO_FORMAT = '%Y-%m-%dT%H:%M:%S'
 
-
+@register.filter()
 def l2l_dt(value):
     if type(value) == datetime:
         return value.strftime(DATE_FORMAT)
     else:
         return (datetime.strptime(value, ISO_FORMAT)).strftime(DATE_FORMAT)
 
-
-
-
-register.filter('l2l_dt', l2l_dt)
+# register.filter('l2l_dt', l2l_dt)
